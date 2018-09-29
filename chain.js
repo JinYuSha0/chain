6815276700f226d17c3dd3a996b897cfe5c3a5cf
邵瑾瑜 <1009943858@qq.com>
2018-09-30 02:03:34 +0800

    first commit

---
+++ chain.js
@@ -0,0 +1,44 @@
+/**
+ * @author JinYu Shao
+ * @date 2018/9/30
+ * @fileName chain.js
+ * @Description:
+*/
+
+function chain(constructor, protoList, returnThis, setInFunc) {
+    if (returnThis || setInFunc) {
+        protoList.forEach(function (p) {
+            for (var i in p) {
+                (function () {
+                    if (setInFunc && !returnThis) {
+                        constructor[i] = p[i]
+                    }
+                    if (returnThis) {
+                        var applyMethod = p[i]
+                        p[i] = function () {
+                            const res = applyMethod.apply(this, arguments)
+                            return res == null ? this : res
+                        }
+                        if (setInFunc) {
+                            constructor[i] = p[i]
+                        }
+                    }
+                })()
+            }
+        })
+    }
+    protoList.reverse()
+    return protoList.reduce(function (ctr, prototype) {
+        var prev = ctr
+        if (protoList.indexOf(prototype) === protoList.length - 1) {
+            ctr = constructor
+        } else {
+            ctr = new Function
+        }
+        if (prev) {
+            ctr.prototype = Object.create(prev.prototype)
+        }
+        Object.assign(ctr.prototype, prototype)
+        return ctr
+    }, null)
+}
