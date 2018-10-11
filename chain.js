6a31c1c044f3075d276828a04f07d9bd94b1b085
o-jingyu.shao <o-jingyu.shao@dji.com>
2018-10-11 19:53:42 +0800

    update

--- chain.js
+++ chain.js
@@ -6,39 +6,40 @@
 */
 
 function chain(constructor, protoList, returnThis, setInFunc) {
-    if (returnThis || setInFunc) {
-        protoList.forEach(function (p) {
-            for (var i in p) {
-                (function () {
-                    if (setInFunc && !returnThis) {
-                        constructor[i] = p[i]
-                    }
-                    if (returnThis) {
-                        var applyMethod = p[i]
-                        p[i] = function () {
-                            const res = applyMethod.apply(this, arguments)
-                            return res == null ? this : res
-                        }
-                        if (setInFunc) {
-                            constructor[i] = p[i]
-                        }
-                    }
-                })()
+  if (returnThis || setInFunc) {
+    protoList.forEach(function (p) {
+      for (var i in p) {
+        (function () {
+          if (setInFunc && !returnThis) {
+            constructor[i] = p[i]
+          }
+          if (returnThis) {
+            var applyMethod = p[i]
+            p[i] = function () {
+              var res = applyMethod.apply(this, arguments)
+              return res == null ? this : res
             }
-        })
+            if (setInFunc) {
+              constructor[i] = p[i]
+            }
+          }
+        })()
+      }
+    })
+  }
+  protoList.reverse()
+  return protoList.reduce(function (ctr, prototype) {
+    var prev = ctr
+    if (protoList.indexOf(prototype) === protoList.length - 1) {
+      ctr = constructor
+    } else {
+      ctr = new Function
     }
-    protoList.reverse()
-    return protoList.reduce(function (ctr, prototype) {
-        var prev = ctr
-        if (protoList.indexOf(prototype) === protoList.length - 1) {
-            ctr = constructor
-        } else {
-            ctr = new Function
-        }
-        if (prev) {
-            ctr.prototype = Object.create(prev.prototype)
-        }
-        Object.assign(ctr.prototype, prototype)
-        return ctr
-    }, null)
+    if (prev) {
+      prev.prototype.constructor = constructor
+      ctr.prototype = Object.create(prev.prototype)
+    }
+    Object.assign(ctr.prototype, prototype)
+    return ctr
+  }, null)
 }
