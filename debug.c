8bebf84f129c49333237921b0b8ad4f374eaf71e
o-jingyu.shao <o-jingyu.shao@dji.com>
2018-10-23 21:01:27 +0800

    update

---
+++ debug.c
@@ -0,0 +1,9 @@
+void consoleLog (int content);
+
+int sumOfSquare (int a, int b)
+{
+  int t1 = a * a;
+  int t2 = b * b;
+  consoleLog(666);
+  return t1 + t2;
+}
--- test.c
+++
@@ -1,5 +0,0 @@
-int sumOfSquare (int a, int b) {
-  int t1 = a * a;
-  int t2 = b * b;
-  return t1 + t2;
-}
